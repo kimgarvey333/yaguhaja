829cd8ee80f536de40c91b74904cfd19b04b36a8
pc-17 <pc-17@pc-17-PC>
2019-06-22 09:07:12 +0900

    로그아웃 서블릿 추가

---
+++ src/yaguhaja/member/member_logout.java
@@ -0,0 +1,21 @@
+package yaguhaja.member;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/member_logout")
+public class member_logout extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.removeAttribute("nickname");
+		response.sendRedirect("index.html");
+	}
+
+}
