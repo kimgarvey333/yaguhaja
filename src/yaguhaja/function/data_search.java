1cf2186b0ce08db40e8800c9b070ee330a3e4d5f
pc-17 <pc-17@pc-17-PC>
2019-06-22 11:36:57 +0900

    검색결과 미완성

--- src/yaguhaja/function/data_search.java
+++ src/yaguhaja/function/data_search.java
@@ -1,6 +1,9 @@
 package yaguhaja.function;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,8 +17,12 @@ public class data_search extends HttpServlet {
 		request.setCharacterEncoding("euc-kr");
 		String search = request.getParameter("search");
 		dataDAO dao = new dataDAO();
-		dao.dataSearch(search);
-		
+		ArrayList<dataDTO> list = dao.dataSearch(search);
+		request.setAttribute("search", search);
+		request.setAttribute("list",list);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("data_search.html");
+	    dispatcher.forward(request, response);
+
 	}
 
 }
